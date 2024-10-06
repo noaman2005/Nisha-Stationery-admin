a1a945557125195887362141f793d304353d5b0c
Noaman Hakim <g22.noaman.imran@gnkhalsa.edu.in>
2024-10-06 19:19:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ module.exports = {
     if (!isServer) {
       // Exclude Node.js modules from the client bundle
       config.resolve.fallback = {
+        child_process: false,
+        fs: false,
+        dns: false,
         net: false,
         tls: false,
         'timers/promises': false,
